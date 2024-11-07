2b1c4639f9b8663639e4c9fa3fd0d3da81613464
tanjiro <counter23062001@mail.ru>
2024-11-07 14:04:54 +0300

    add project

---
+++ materials/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from users.models import NULLABLE
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=150, verbose_name="название")
+    preview = models.ImageField(
+        upload_to="materials/courses_previews/", verbose_name="превью", **NULLABLE
+    )
+    description = models.TextField(verbose_name="описание", **NULLABLE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "курс"
+        verbose_name_plural = "курсы"
+
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, verbose_name="курс", on_delete=models.CASCADE, **NULLABLE)
+    name = models.CharField(max_length=150, verbose_name="название")
+    description = models.TextField(verbose_name="описание", **NULLABLE)
+    preview = models.ImageField(
+        upload_to="materials/lessons_previews/", verbose_name="превью", **NULLABLE
+    )
+    link = models.CharField(max_length=250, verbose_name="ссылка", **NULLABLE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "урок"
+        verbose_name_plural = "уроки"
