620d039e8ff7ee70867e5a366dde020fbee686d8
tanjiro <counter23062001@mail.ru>
2024-11-09 22:25:14 +0300

    update

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import UserCreateAPIView, UserUpdateAPIView, UserListAPIView, UserRetrieveAPIView
+from users.views import (UserCreateAPIView, UserUpdateAPIView, UserListAPIView, UserRetrieveAPIView, PaymentListAPIView, PaymentCreateAPIView)
 
 app_name = UsersConfig.name
 
@@ -11,5 +11,7 @@ urlpatterns = [
     path("create/", UserCreateAPIView.as_view(), name='user_create'),
     path("<int:pk>/update/", UserUpdateAPIView.as_view(), name='user_update'),
     path("<int:pk>/", UserRetrieveAPIView.as_view(), name='user_view'),
+    path("payments/", PaymentListAPIView.as_view(), name='payment_list'),
+    path("payments/create/", PaymentCreateAPIView.as_view(), name='payment_create')
 ]
 
