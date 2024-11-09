b2a0729f2cc34113ef7ba7231a79af536b8b0dbf
tanjiro <counter23062001@mail.ru>
2024-11-09 22:25:41 +0300

    add payment's views

--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from rest_framework import generics
-from users.serializers import UserSerializer
-from users.models import User
+from users.serializers import (UserSerializer, PaymentSerializer, UserRetrieveSerializer)
+from users.models import User, Payment
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
 
 class UserUpdateAPIView(generics.UpdateAPIView):
@@ -23,6 +25,17 @@ class UserCreateAPIView(generics.CreateAPIView):
 
 
 class UserRetrieveAPIView(generics.RetrieveAPIView):
-    serializer_class = UserSerializer
+    serializer_class = UserRetrieveSerializer
     queryset = User.objects.all()
 
+
+class PaymentListAPIView(generics.ListAPIView):
+    serializer_class = PaymentSerializer
+    queryset = Payment.objects.all()
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    ordering_fields = ("payment_date",)
+    filterset_fields = ("paid_course", "paid_lesson", "payment_method")
+
+
+class PaymentCreateAPIView(generics.CreateAPIView):
+    serializer_class = PaymentSerializer
